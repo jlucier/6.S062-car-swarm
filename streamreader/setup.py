6e978cdbf21fee7effe8a8ccb779a810b6839635
Jordan Lucier <jordan@jordanlucier.com>
2016-04-22 20:42:39 -0400

    Cleaned up setup.py

--- streamreader/setup.py
+++ streamreader/setup.py
@@ -3,9 +3,6 @@ from distutils.core import setup, Extension
 streamreader = Extension('streamreader',
 					language = 'c++',
                     sources = ['streamreader.cpp'],
-                    include_dirs = ['/home/jordan/Desktop/6.S062-car-swarm/streamreader/libs/ViconDataStreamSDK_CPP',
-                    				'DebugServices'],
-                    library_dirs = ['/home/jordan/Desktop/6.S062-car-swarm/streamreader/libs'],
                     libraries = ['boost_python', 'ViconDataStreamSDK_CPP'])
 
 setup (name = 'Stream Reader',
