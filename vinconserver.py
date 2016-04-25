befb0356b01b2766786a5f3568edcd1d3979dacb
Jordan Lucier <jordan@jordanlucier.com>
2016-04-25 03:14:45 -0400

    First attempt at vicon streaming server

--- vinconserver.py
+++ vinconserver.py
@@ -1,37 +1,64 @@
-from SocketSever import *
+from SocketServer import *
 import socket, threading
+import json
+import math
+
+from collections import deque
 
 import streamreader
 
-SERVER_PORT = 1001
+SERVER_PORT = 4001
 VICON_PORT = 801
 VICON_HOST = '192.168.20.99'
 
 class ViconRequestHandler(BaseRequestHandler):
 
 	def handle(self):
-		pass		
+		self.request.sendall(json.dumps(self.server.get_most_recent_frame()))
 
 class ViconServer(ThreadingMixIn, TCPServer):
 
 	def __init__(self):
-		super(TCPServer, self).__init__((socket.gethostname(), PORT), ViconRequestHandler)
+		TCPServer.__init__(self, (socket.gethostname(), SERVER_PORT), ViconRequestHandler)
 
-		self.vicon_thread = None
+		self.vicon_thread = threading.Thread(target=self.read_vicon_stream)
+		self.stop_vicon = False
+		self.frames = deque(maxlen=10)
 
-		if !streamreader.connect(VICON_HOST):
+		if not streamreader.connect(VICON_HOST):
 			raise Exception("Couldn't connect to vicon system at : {}:{}".format(VICON_HOST, VICON_PORT))
 
-		# set up frame grabbing
+	def read_vicon_stream():
+		while not self.stop_vicon:
+			curr_frame = streamreader.get_frame()
+			new_frame = dict()
+			if len(self.frames) != 0:
+				for car, values in curr_frame.iteritems():
+					v = math.sqrt(abs(values[0] - self.frames[-1][0])**2 + abs(values[1] - self.frames[-1][1])**2)
+					new_frame[car] = (values[0], values[1], values[2], v, values[3])
+			else:
+				for car, values in curr_frame.iteritems():
+					new_frame[car] = (values[0], values[1], values[2], 0, values[3])
+			self.frames.append(new_frame)
+
+	def get_most_recent_frame():
+		return self.frames[-1]
 
 	def start(self):
 		t = threading.Thread(target=self.serve_forever)
 		t.daemon = True
 		t.start()
-		# TODO start getting frames from vicon
+		self.vicon_thread.start()
 		return t
 
 	def stop(self):
-		# TODO kill vicon
+		self.stop_vicon = True
 		self.shutdown()
 		self.server_close()
+		return True
+
+def main():
+	test =  ViconServer()
+
+if __name__ == '__main__':
+	main()
